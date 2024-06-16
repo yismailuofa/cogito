be54b9827d2e2bbc22e8f387936b391c63436d53
Youssef Ismail <yismail@ualberta.ca>
2024-06-16 03:22:53 +0300

    Init

---
+++ bookmarklet.js
@@ -0,0 +1,35 @@
+javascript: (function () {
+  /** Replace these variables with your own values */
+  const apiUrl = "https://api.github.com/repos/yismailuofa/cogito/issues";
+  const token = "YOUR_GITHUB_TOKEN";
+
+  const title = document.title ?? "No title";
+  const body = window.location.href ?? "No URL";
+
+  const issueData = {
+    title,
+    body,
+    labels: ["todo"],
+  };
+
+  fetch(apiUrl, {
+    method: "POST",
+    headers: {
+      Authorization: `token ${token}`,
+      Accept: "application/vnd.github.full+json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(issueData),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.html_url) {
+        alert(`Issue created: ${data.html_url}`);
+      } else {
+        alert(`Error creating issue: ${data.message}`);
+      }
+    })
+    .catch((error) => {
+      alert(`Network error: ${error}`);
+    });
+})();
